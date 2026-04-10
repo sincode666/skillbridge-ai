7e4f025b5847459a10d6eeaec874ab91cdf50bf3
Shashank <shashank.chauhan.ai@gmail.com>
2026-04-10 13:20:53 +0530

    Initial commit - user accounts authentication backend

---
+++ user_accounts/serializers.py
@@ -0,0 +1,32 @@
+# from django.contrib.auth.models import User
+# from rest_framework import serilaizers
+
+# class RegisterSerializer(serilaizers.ModelSerializer):
+#     class Meta:
+#         model = User
+#         fields=['username','password','mail']
+'''it was not importent to write  if their is anyother function is called instead for User 
+    but becasue we have to store password in hassed formed so we hav to use it with
+    "create_user" it store the pass in hased form
+    It get's auomatically called then .save() is called
+    as **validate_data takes all the values and variables to store and valedate the data'''
+    # def create(self, validated_data):
+    #     return User.objects.create_user(**validated_data)
+'''To be more accurate'''
+from rest_framework import serializers
+from django.contrib.auth.models import User
+
+class RegisterSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True)
+
+    class Meta:
+        model = User
+        fields = ['username','password','email']
+
+    def create(self, validated_data):
+        user = User.objects.create_user(
+            username=validated_data['username'],
+            password=validated_data['password'],
+            email=validated_data['email']
+        )
+        return user
---
+++ user_accounts/views.py
@@ -0,0 +1,34 @@
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework_simplejwt.tokens import RefreshToken
+from django.contrib.auth import authenticate
+from .serializers import RegisterSerializer
+
+class RegisterView(APIView): #here apiview we can say that it takes the method init and convert it into the function we use it as <classname>.as_view() in endpoint
+    def post(self, request):
+        serializer = RegisterSerializer(data=request.data)
+        if serializer.is_valid():
+            user = serializer.save()
+            refresh = RefreshToken.for_user(user)
+            return Response({
+                'access': str(refresh.access_token),
+                'refresh': str(refresh),
+            }, status=201)
+        return Response(serializer.errors, status=400)
+
+
+class LoginView(APIView):
+    def post(self, request):
+        username = request.data.get('username')
+        password = request.data.get('password')
+        #get the username and password from the db and checks if it is present or not
+        user = authenticate(username=username, password=password)
+        if user:
+            refresh = RefreshToken.for_user(user)
+            return Response({
+                'login':'corect',
+                'access': str(refresh.access_token),
+                'refresh': str(refresh),
+            })
+        return Response({'error': 'Invalid credentials'}, status=status.HTTP_401_UNAUTHORIZED)
